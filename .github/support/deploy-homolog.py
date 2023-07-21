30738c16f0758380aaaa13256dcf2b2894978ab8
Robson P Oliveira <rpopuc@gmail.com>
2023-07-21 16:48:21 -0300

    fix fix

--- .github/support/deploy-homolog.py
+++ .github/support/deploy-homolog.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import json
 
 errors = []
 
@@ -15,12 +16,40 @@ def parseBranches(payload):
     # strip whitespaces from each branch
     return [branch.strip() for branch in branches]
 
+def getBranches(currentList, mergeList):
+    currentList = currentList.split()
+
+    # Strip matches by end line character and get an array of branches
+    matches = re.search("##\s+branches\s*([^#]*)", mergeList, re.IGNORECASE + re.MULTILINE)
+    branches = [branch.strip() for branch in matches.group(1).splitlines()]
+    branches = list(filter(None, branches))
+
+    # percorra branches e verifique as que contém _DELETE_ no nome. Adicione em deleteBranches e remova-as de branches
+    deleteBranches = []
+    for branch in branches:
+        if '_DELETE_' in branch:
+            deleteBranches.append(branch.replace('_DELETE_', '').strip())
+        else:
+            currentList.append(branch.strip())
+
+    # remove from currentList the branches that are in deleteBranches
+    for branch in deleteBranches:
+        if branch in currentList:
+            currentList.remove(branch)
+
+    return currentList
+
 try:
     command = sys.argv[1]
-    payload = sys.argv[2]
+    currentList = sys.argv[2]
+    mergeList = sys.argv[3]
 
     if command == 'parse-branches':
-        print('\n'.join(parseBranches(payload)))
+        print('\n'.join(parseBranches(mergeList)))
+        exit()
+
+    if command == 'get-branches':
+        print('\n'.join(getBranches(currentList, mergeList)))
         exit()
 
     print('["invalid-command"]')
