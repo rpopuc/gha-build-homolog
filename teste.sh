3621305bbfd81f60df60fb8883b3f626345581ff
Robson P Oliveira <rpopuc@gmail.com>
2023-07-21 08:01:22 -0300

    fix: verify blocked

--- teste.sh
+++ teste.sh
@@ -1,3 +1,7 @@
-lista_branches=$(cat lista)
-echo $lista_branches
-lista_limpa=$(echo "$lista_branches" | tr ' ' '\n' | sort -u)
+curl \
+  --request GET \
+  --silent \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_ACCESS_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  --url "https://api.github.com/repos/rpopuc/gha-build-homolog/actions/workflows/deploy.yaml/runs" | jq -r '.workflow_runs[0].status'
