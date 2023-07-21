caa68772668a9b7e20e4f1068da13d16208b215c
Robson P Oliveira <rpopuc@gmail.com>
2023-07-21 14:16:11 -0300

    update develop action

--- .github/support/deploy-environment.sh
+++ .github/support/deploy-environment.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
-$lista_branches=$1
-git checkout develop
-git branch -D homolog
-git checkout -b homolog develop
+lista_branches=$1
+git config --global user.email "robot@github.com"
+git config --global user.name "Homolog Robot"
+git checkout develop &> /dev/null
+git branch -D homolog &> /dev/null
+git checkout -b homolog develop &> /dev/null
 nova_lista=$lista_branches
-echo "DEBUG: Entrando no loop"
 while read -r branch; do
   set +e
-  echo "DEBUG: Verifica se $branch existe"
   git ls-remote --exit-code --heads origin $branch &> /dev/null
   branch_exists=$?
-  echo "DEBUG: Verificou se $branch $branch_exists"
   branch_merged=0
   if [[ "$branch_exists" == "0" ]]; then
-    git checkout $branch
+    git checkout $branch &> /dev/null
     git log --pretty=format:"%h" develop | grep `git log --pretty=format:"%h" -1 $branch` &> /dev/null
     branch_merged=$?
   fi
   if [[ "$branch_exists" == "0" ]] && [[ "$branch_merged" != "0" ]]; then
     set -e
-    git rebase develop
-    git checkout homolog
-    git merge $branch --no-edit
+    git rebase develop &> /dev/null
+    git checkout homolog &> /dev/null
+    git merge $branch --no-edit &> /dev/null
   else
     set -e
-    if [[ "$branch_exists" != "0" ]]; then
-      echo "DEBUG: Branch $branch não existe!!"
-    fi
     nova_lista="${nova_lista//$branch/}"
   fi
 done <<< "$lista_branches"
-echo "Preparando homolog"
-git checkout homolog
+git checkout homolog &> /dev/null
 echo $nova_lista | tr ' ' '\n' > homolog-branches
-echo "Atualizando lista"
 git add .
-git commit -m "Atualizando homolog com $nova_lista"
-git push --force origin homolog
+git commit -m "Atualizando homolog com $nova_lista" &> /dev/null
+git push --force origin homolog &> /dev/null
+echo "sucesso"
